a18f4d781fe9b0760f0daa1367dc920ec97d912e
vagrant <vagrant@homestead>
2022-01-22 14:07:27 +0000

    基础页面结构

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        // config/app.php  'providers'=>  [  ... Sven\ArtisanView\ServiceProvider::class,  ];
+        Sven\ArtisanView\ServiceProvider::class,
     ],
 
     /*
